9ff6d5bf80859633eb34ddd3376c5ebcf9558967
REBradley <reb337@cornell.edu>
2016-10-21 22:53:52 -0700

    testing

--- winearb/reviews/urls.py
+++ winearb/reviews/urls.py
@@ -11,13 +11,4 @@ urlpatterns = [
     url(r'^review/edit_review/(?P<review_id>[0-9]+)/$', views.edit_review, name='edit_review'),
     url(r'^review/delete_review/(?P<review_id>[0-9]+)/$', views.delete_review, name='delete_review'),
     url(r'^detail/(?P<review_id>[0-9]+)/$', views.wine_detail, name='wine_detail'),
-
-
-
-    #url(r'^wine/(?P<wine_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
-
-
-
-    url(r'^wine$', views.wine_list, name='wine_list'),
-    url(r'^recommendation/$', views.user_reccommendation_list, name = 'user_recommendation_list'),
 ]
