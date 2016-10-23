1f485f6b1e767ad1f7c9deed4582e8798d8f23d1
REBradley <reb337@cornell.edu>
2016-10-23 03:06:51 -0700

    testing to get S3 working

--- winearb/upload_handling/tests/factories.py
+++ winearb/upload_handling/tests/factories.py
@@ -4,15 +4,27 @@ from ..models import *
 from ...users.tests.factories import UserFactory
 from ...reviews.tests.factories import ReviewFactory
 
-import os
+### For grabbing a test image. ###
+import requests
+from PIL import Image
+from io import BytesIO
+
+def test_image():
+    get_wine_label = requests.get('https://thewinecountry.com/twcwp/wp-content/uploads/2015/04/chateau-margaux-1982-margaux-bordeaux-300x300.jpg')
+    image_file = BytesIO(get_wine_label.content)
+    return image_file
 
 
 class WineImageFactory(factory.django.DjangoModelFactory):
+    """
+    Test the creation of WineImages. Specifically, we want to make sure that this
+    is properly loaded onto Amazon S3. Therefore, it needs to be able to run on our production server.
+    """
     class Meta:
         model = WineImage
 
     review = factory.SubFactory(ReviewFactory)
     user = factory.SubFactory(UserFactory)
     shot = factory.django.ImageField(
-        from_path= '{}/winearb/media/initial_photos/tour-haut-caussan-label.jpg'.format(str(os.getcwd())),
+        from_file=test_image(),
     )
