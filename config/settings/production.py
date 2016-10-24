8c577b6de588628d2f846f083098352ab78bbfe0
REBradley <reb337@cornell.edu>
2016-10-23 19:15:00 -0700

    testing to get S3 working. solution found.almost

--- config/settings/production.py
+++ config/settings/production.py
@@ -106,7 +106,7 @@ AWS_HEADERS = {
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 
-MEDIA_URL = '%s.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 # Static Assets
 # ------------------------
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
